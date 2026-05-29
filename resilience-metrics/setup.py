2d850247239a17a410d28d5afbc87c421c88e879
tarek-clarke <tarek.clarke15@gmail.com>
2026-05-29 13:23:11 -0400

    Upload and track open-source resilience-metrics package

---
+++ resilience-metrics/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="resilience-metrics",
+    version="1.0.0",
+    description="Established system resilience scoring metrics for TKDE paper evaluation",
+    author="Academic Research",
+    packages=find_packages(),
+    python_requires=">=3.10",
+    install_packages=[],
+)
